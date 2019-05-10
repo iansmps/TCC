1e6e060ccfc17d32f4041c0403d836ea21620ede
iansmps <iansmps@gmail.com>
2019-05-10 06:10:09 +0000

    "logs 09/05"

--- scriptgit.py
+++ scriptgit.py
@@ -16,7 +16,7 @@ from collections import deque
 command = ['git','add','-A']
 subprocess.call(command)
 
-command2 = ['git','commit','-m','"logs 08/05"']
+command2 = ['git','commit','-m','"logs 09/05"']
 subprocess.call(command2)
 
 command3 = ['git','push','-u', 'origin','master']
